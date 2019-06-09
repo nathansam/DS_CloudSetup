95be0341c0b7d40aaaecea4aa44c5c00469f7f27
nathansam <ncooke96@gmail.com>
2019-06-09 16:49:26 +0100

    the first commit

---
+++ cloudscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
+sudo apt update
+sudo apt install r-base
+sudo apt-get install gdebi-core
+wget https://download2.rstudio.org/server/trusty/amd64/rstudio-server-1.2.1335-amd64.deb
+sudo gdebi rstudio-server-1.2.1335-amd64.deb
+adduser nathan
