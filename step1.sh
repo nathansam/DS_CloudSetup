2963a00779ba1219aabc128ade6022e96842f5db
nathansam <ncooke96@gmail.com>
2019-07-03 16:08:37 +0100

    Add dependencies

--- step1.sh
+++ step1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #install R
+
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9 # Add key
 sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/' # Add Repo
 sudo apt-get -y update
