9f70ad1851fce96a1b1302b01aceec73c96b0a63
Nathan Constantine-Cooke <ncooke96@gmail.com>
2019-08-31 19:58:08 +0100

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -5,8 +5,8 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD
 sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/' # Add Repo
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt install -y r-base #Install R
-sudo apt-get install -y gdebi-core #install gdebi for installing rstudio-server
+sudo apt install -y r-base # Install R
+sudo apt-get install -y gdebi-core # Install gdebi for installing rstudio-server
 
 
 pip3 install virtualenv numpy scipy scikit-learn pandas matplotlib
@@ -20,6 +20,7 @@ sudo apt-get install -y libcurl4-openssl-dev
 #Install dependencies for R packages
 sudo apt-get -y install libcurl4-gnutls-dev
 sudo apt-get -y install libssl-dev
+sudo apt-get install -y libxml2-dev # Required for roxygen2
 sudo apt-get install m4
 wget https://gmplib.org/download/gmp/gmp-6.1.2.tar.xz
 sudo apt-get install -y xz-utils
